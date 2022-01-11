1b465b8e6915c910a9d2074b9a51adb43298008c
Cybex-developers <Cybex.developers@gmail.com>
2022-01-11 12:29:43 +0530

    commit

---
+++ README.md
@@ -0,0 +1,9 @@
+How to use cybex-web2exe
+
+make a settings.json file in the root of the app
+
+write "app":"HTMLfileLocation" and type
+
+settings.json might look like {"app":"./index.html"}
+
+cwe --run to test your app
--- app/index.js
+++ app/index.js
@@ -1,19 +1,38 @@
 #!/usr/bin/env node
 
 const arg = process.argv;
-if (arg.includes("--run")) {
-    const {launch} = require('chrome-runner');
-    const path = require("path")
-    __dirname = path.resolve();
 
-    const runner = launch(
-        {"startupPage": __dirname + "/public/index.html",
-        "chromeFlags": ["--app="+ __dirname +"/public/index.html"]
-    });
-    setTimeout(() => {
-        process.exit()
-    }, 2);
+if (arg.includes("--run")) {
+    const fs = require('fs');
+    if (fs.existsSync('./settings.json')) {
+        const data = JSON.parse(fs.readFileSync("./settings.json"));
+
+        if (data.app != null) {
+            if (fs.existsSync(data.app)) {
+                const {launch} = require('chrome-runner');
+                const path = require("path")
+                __dirname = path.resolve();
+            
+                const runner = launch(
+                    {"startupPage": __dirname + data.app,
+                    "chromeFlags": ["--app="+ __dirname + data.app]
+                });
+                setTimeout(() => {
+                    process.exit()
+                }, 2);
+            }
+            else{
+                console.log("Please specify the correct html file location.");
+            }
+        }
+        else{
+            console.log("Please specify the html file using 'app':'fileLocation'");
+        }
+    }
+    else{
+        console.log("settings.json does not exist \nCreate a settings.json to continue \nfor more details kindly go to the official website");
+    }
 }
-if (arg.includes("--version")) {
+if (arg.includes("--v") || arg.includes("--version")) {
     console.log("v2022.1");
 }
